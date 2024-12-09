86bce30d138165e71699d386cab5584c838ac216
Alexander Erbiste <alexander.erbiste@gmail.com>
2024-12-09 10:57:06 -0300

    first commit

---
+++ App/Views/home/index.php
@@ -0,0 +1,6 @@
+    <div class="starter-template">
+        <h1>Bem Vindos ao meu projeto MVC</h1>
+        <p class="lead">Alexander Ribeiro Erbiste Filho 2 TDSN</p>
+        <a href="https://github.com/AlexanderErbiste/ProjetoPHP/archive/refs/heads/main.zip" class="btn btn-success btn-sm">GIT Download</a>
+        <a href="https://github.com/AlexanderErbiste/ProjetoPHP/tree/main/htdocs-main" target="_blank" class="btn btn-info btn-sm">GIT Project</a>
+    </div>
